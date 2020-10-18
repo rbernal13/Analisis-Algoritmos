72d6fe5e7ab10bc71f2821e03bdde89157012752
Ricardo Bernal Alfonso <RicardoBernalAlfonso@users.noreply.github.com>
2020-10-18 18:40:45 -0500

    Taller 3 - E1

--- Talleres/Taller 3/ejercicio1_suma134.py
+++ Talleres/Taller 3/ejercicio1_suma134.py
@@ -10,4 +10,16 @@ def sumarFormas(n):
         count += sumarFormas(n-arr[j])
     return count
 
-print(sumarFormas(7))
+def sumarNumero(n):
+    suma = [0 for i in range(0, n + 1)] 
+
+    suma[0] = suma[1] = suma[2] = 1
+    suma[3] = 2
+    
+    for i in range(4, n + 1): 
+        suma[i] = suma[i - 1] + suma[i - 3] + suma[i - 4] 
+      
+    return suma[n] 
+
+
+print(sumarNumero(7))
