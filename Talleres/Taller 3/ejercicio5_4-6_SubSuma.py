29d007326f0115eacd3036bf12916fc8caddd2e4
Ricardo Bernal Alfonso <RicardoBernalAlfonso@users.noreply.github.com>
2020-10-18 21:08:50 -0500

    Taller 3 - SubSuma arreglado

--- Talleres/Taller 3/ejercicio5_4-6_SubSuma.py
+++ Talleres/Taller 3/ejercicio5_4-6_SubSuma.py
@@ -6,27 +6,36 @@ def subsecuencia_suma (S,n):
       return 0
     subK  = K // 2 # Suma de una subsecuencia
 
-    P = [[False for j in range(subK+1)] for i in range(n+1)] 
+    subP = [[False for j in range(n+1)] for i in range(subK+1)] 
 
     i, j = 0, 0
 
-    for i in range(1,n+1):
-        P [i][0] = True   # En el numero 0, relleno con True
+    for i in range(0,n+1):
+        subP [0][i] = True   # En el numero 0, relleno con True
 
-    for i in range (1,n+1): # Recorro cada fila
-      for j in range (1,subK+1): # Recorro cada columna 
-        if (j < S[i-1]):
-          P[i][j] = P[i-1][j]
-        else:
-          P[i][j] = (P[i-1][j] or P[i-1][j - S[i-1]])
-    return P[n][subK]
+    for i in range (1,subK+1): # Recorro cada fila
+      for j in range (1,n+1): # Recorro cada columna 
+          subP[i][j] = subP[i][j-1]
+          if i >= S[j-1]:
+            subP[i][j] = (subP[i][j] or subP[i-S[j-1]][j - 1])
+    return subP[subK][n]
 
-#S = [2,1,1,1,3,1]
-S = [2,1,1,1,3,2]
-n = len(S)
-res = subsecuencia_suma(S,n)
-if res == True:
-  print("se puede")
-else:
-  print("paila")
+def subsecuenciaSum(S):
+  n = len(S)
+  res = subsecuencia_suma(S,n)
+  if res == True:
+    print("se puede")
+  else:
+    print("paila")
+
+S = [2,1,1,1,3,1]
+S1 = [2,1,1,1,3,2]
+
+print("Para la secuencia S ", S)
+subsecuenciaSum(S)
+
+print("Para la secuencia S ", S1)
+subsecuenciaSum(S1)
+
+
 
